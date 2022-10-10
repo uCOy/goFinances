ce28fe5262009f32492201e293b7e4c7e9927e6d
RICHARD HENRIQUE COUTO DA ROCHA <richard.hcrocha@senacsp.edu.br>
2022-10-10 19:15:41 -0300

    Toma Theo

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { ThemeProvider } from 'styled-components';
+
+import theme from './src/global/styles/theme';
+
+import { Dashboard } from './src/screens/dashboard/index';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  })
+  if(!fontsLoaded) return null;
+
+  SplashScreen.hideAsync();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard/>
+    </ThemeProvider>
+  );
+}
+
+
