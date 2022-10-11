283fc99c6613819e1279fdcf707716611960a70e
RICHARD HENRIQUE COUTO DA ROCHA <richard.hcrocha@senacsp.edu.br>
2022-10-10 22:09:24 -0300

    goFinances Pega

--- src/screens/dashboard/index.tsx
+++ src/screens/dashboard/index.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
-import { Container, Title }  from './styles'; 
+import { HighlightCard } from '../../components/HighlightCard';
+import { 
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreething,
+  UserName,
+  UserWrapper,
+  Icon,
+  HighlightCards,
+}  from './styles'; 
 
 export function Dashboard(){
   return(
     <Container>
-      <Title>
-        Dashboard...
-      </Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo 
+              source={ { uri:'https://avatars.githubusercontent.com/u/66300419?v=4'} }
+              />
+            <User>
+              <UserGreething>Olá,</UserGreething>
+              <UserName>Richard</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+      <HighlightCards>
+        <HighlightCard />
+        <HighlightCard />
+        <HighlightCard />
+      </HighlightCards>
     </Container>
   )
 }
--- src/screens/dashboard/styles.ts
+++ src/screens/dashboard/styles.ts
@@ -1,13 +1,64 @@
 import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { Feather } from '@expo/vector-icons';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
   background-color: ${( {theme} ) => theme.colors.background};
 `;
-export const Title = styled.Text`
-  font-size: 24px;
+
+// export const Title = styled.Text`
+//   font-size: 24px;
+//   font-family: ${( {theme} ) => theme.fonts.bold};
+//   color: ${( {theme} ) => theme.colors.purple};
+// `;
+
+export const Header = styled.View`
+  width: 100%;
+  background-color: ${( {theme} ) => theme.colors.purple};
+  height: ${RFPercentage(42)}px; // height: ${RFPercentage(36)}px;
+`;
+
+export const UserWrapper= styled.View`
+  width: 100%;
+  padding: 0 24px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: ${RFValue(58)}px;
+`;
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Photo = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+  border-radius: 10px;
+`;
+
+export const User = styled.View`
+  margin-left: 17.13px;
+`;
+
+export const UserGreething = styled.Text`
+  color: ${( {theme} ) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
+`;
+
+export const UserName = styled.Text`
+  color: ${( {theme} ) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
   font-family: ${( {theme} ) => theme.fonts.bold};
-  color: ${( {theme} ) => theme.colors.purple};
+`;
+
+export const Icon = styled(Feather)`
+  color: ${( {theme} ) => theme.colors.orange};
+  font-size: ${RFValue(24)}px;
+`;
+
+export const HighlightCards = styled.View`
+  flex-direction: row;
 `;
